6453ad05bbab278a4aa319ac18c71cb362db4f18
juan25 <elbartoy@gmail.com>
2026-07-07 21:36:34 -0600

    Se arreglo el problema y ya no es problema

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',        
+    host: "localhost",   
     port: 5173,
-    strictPort: true,
-    cors: true         
+    strictPort: true
   }
 })
